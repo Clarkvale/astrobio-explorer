757abc41f8c29b2e5812e4fc2b73d9f6257731e3
Benjamin Clark <60831788+Clarkvale@users.noreply.github.com>
2021-10-15 16:11:55 -0400

    added licence description

--- app.R
+++ app.R
@@ -1,3 +1,16 @@
+#License
+
+# Â© Copyright 2021 iGEM Concordia, Benjamin Clark
+# This file is part of AstroBio Explorer.
+# AstroBio Explorer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or any later version.
+# AstroBio is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with AstroBio Explorer.  If not, see <https://www.gnu.org/licenses/>.
 
 library(shiny)
 library(shinydashboard)
